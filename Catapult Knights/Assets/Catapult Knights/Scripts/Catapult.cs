3b20dd548e1c8c47aade9ec6742c16c18f914084
CarlKaste <117178619+CarlKaste@users.noreply.github.com>
2022-12-15 16:29:31 +0100

    Fixed Socket interactor size

--- Catapult Knights/Assets/Catapult Knights/Scripts/Catapult.cs
+++ Catapult Knights/Assets/Catapult Knights/Scripts/Catapult.cs
@@ -7,13 +7,26 @@ public class Catapult : MonoBehaviour
     [SerializeField]
     private Animator catapultAnimator;
 
+    [SerializeField]
+    private GameObject ammunitionSocketInteractor;
+
+    [SerializeField]
+    private GameObject projectile;
+
     public void ActivateLever()
     {
         catapultAnimator.SetBool("ReadyToShoot", false);
+        ammunitionSocketInteractor.SetActive(false);
     }
 
     public void ReloadWheel()
     {
         catapultAnimator.SetBool("ReadyToShoot", true);
+        ammunitionSocketInteractor.SetActive(true);
+    }
+
+    void ProjectileShoot()
+    {
+
     }
 }
