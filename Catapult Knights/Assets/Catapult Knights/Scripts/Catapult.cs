5cd0fb10c031b9c1dbc56c2ea2082a0539d3438d
CarlKaste <117178619+CarlKaste@users.noreply.github.com>
2022-12-17 17:48:53 +0100

    Added functions to the catapult

--- Catapult Knights/Assets/Catapult Knights/Scripts/Catapult.cs
+++ Catapult Knights/Assets/Catapult Knights/Scripts/Catapult.cs
@@ -11,28 +11,45 @@ public class Catapult : MonoBehaviour
     private Animator catapultGuardAnimator;
 
     [SerializeField]
+    private GameObject projectilePrefab;
+
+    [SerializeField]
+    private GameObject projectileSpawner;
+
+    [SerializeField]
     private GameObject ammunitionSocketInteractor;
 
     [SerializeField]
-    private GameObject projectile;
+    private GameObject ammunitionDestroyer;
+
+    [SerializeField]
+    private float projectileForce;
+
+
 
     public void ActivateLever()
     {
         catapultAnimator.SetBool("ReadyToShoot", false);
-        ammunitionSocketInteractor.SetActive(false);
         catapultGuardAnimator.SetBool("CatapultShoots", true);
-
+        ammunitionSocketInteractor.SetActive(false);
+        ammunitionDestroyer.SetActive(true);
+        StartCoroutine(ShootProjectile());
     }
 
     public void ReloadWheel()
     {
         catapultAnimator.SetBool("ReadyToShoot", true);
-        ammunitionSocketInteractor.SetActive(true);
         catapultGuardAnimator.SetBool("CatapultShoots", false);
+        ammunitionSocketInteractor.SetActive(true);
+        ammunitionDestroyer.SetActive(false);
     }
 
-    void ProjectileShoot()
+    IEnumerator ShootProjectile()
     {
-
+        yield return new WaitForSeconds(0.3f);
+        GameObject spawnedProjectile = Instantiate(projectilePrefab, projectileSpawner.transform);
+        spawnedProjectile.transform.SetParent(null);
+        Rigidbody projectileRigidbody = spawnedProjectile.GetComponent<Rigidbody>();
+        projectileRigidbody.AddForce(projectileSpawner.transform.forward * projectileForce);
     }
 }
